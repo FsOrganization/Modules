80d6500568d25892907e203b890244a83200431e
Administrator <likemiddle@gmail.com>
2015-10-15 23:23:39 +0800

    add

---
+++ src/com/fla/common/security/SessionTimeoutInterceptor.java
@@ -0,0 +1,52 @@
+package com.fla.common.security;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.DefaultServletHttpRequestHandler;
+
+import com.fla.common.entity.SystemUser;
+
+public class SessionTimeoutInterceptor implements HandlerInterceptor {
+//	
+//	@Autowired
+//	public SystemUser systemUser;
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,HttpServletResponse response, Object obj, Exception e)throws Exception {
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response,Object obj, ModelAndView mv) throws Exception {
+	        //response.sendRedirect(request.getContextPath()+"/pages/system/welcome.light");  
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler) throws Exception {
+		SystemUser s = (SystemUser) request.getSession().getAttribute("systemUser");
+		if (s == null || s.getServiceShopCode() == null) {
+			if (request.getHeader("x-requested-with") != null && request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {
+				response.setHeader("sessionstatus", "timeout");
+				response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY); 
+				response.setCharacterEncoding("utf-8");
+				response.setContentType("text/html; charset=utf-8");
+				PrintWriter printWriter = response.getWriter();
+				JSONObject j = new JSONObject();
+				j.put("redirect", request.getContextPath()+"/pages/login.jsp");
+				printWriter.write(j.toString());
+				printWriter.flush();
+				printWriter.close();
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+}
